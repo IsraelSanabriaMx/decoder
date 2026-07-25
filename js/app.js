ffd3b5dbf7b634f448967aa519bdbc68ae897de9
IsraelSanabriaMx <israel.sanabriamx@gmail.com>
2026-07-24 22:46:53 -0600

    feat(-): decoder original version

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const tbody = document.querySelector("#data tbody");
+
+document
+  .getElementById("btnDecode")
+  .addEventListener("click", decode);
+
+document
+  .getElementById("btnClean")
+  .addEventListener("click", clean);
+
+function decode() {
+  tbody.innerHTML = "";
+
+  const input = document
+    .getElementById("input")
+    .value;
+
+  CONFIG.forEach(field => {
+    const end = field.start + field.length;
+    const value = input.substring(
+      field.start,
+      end
+    );
+
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${field.key}</td>
+      <td>${field.name}</td>
+      <td>${field.length}</td>
+      <td class="value">${value}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
+
+function clean() {
+  document.getElementById("input").value = "";
+  tbody.innerHTML = "";
+}
